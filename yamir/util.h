e1996c552af5ac3e12702a200740e33342155ec5
cof <cof@fastmail.fm>
2026-04-22 13:24:43 +0100

    Moved hashmap code from util into hashmap.c

--- yamir/util.h
+++ yamir/util.h
@@ -25,23 +25,4 @@ static inline const char *get_basename(const char *path)
     return base ? base + 1 : path;
 }
 
-/* hashmap api */
-struct inthash_entry {
-    struct inthash_entry *next;
-    uint32_t key;
-    void *data;
-};
-
-struct inthash_table {
-    int size;
-    struct inthash_entry **array;
-};
-
-extern void *inthash_table_lookup(struct inthash_table *table, uint32_t key);
-extern void inthash_table_del(struct inthash_table *table, uint32_t key);
-extern void inthash_table_add(struct inthash_table *table, uint32_t key, void *data);
-extern void inthash_table_free(struct inthash_table *table);
-extern struct inthash_table *inthash_table_create(int size);
-
-
 #endif
