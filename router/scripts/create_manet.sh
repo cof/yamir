929ff97dc7c5e2a8e383c1a6fc6e44a943a0782e
cof <cof@fastmail.fm>
2026-04-30 00:18:58 +0100

    Added create_manet.sh

---
+++ router/scripts/create_manet.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+KYAMIR=/home/alpine/kyamir/kyamir.ko
+YAMIRD=/home/alpine/yamird
+
+# network config 
+NS1="yamir1"
+NS2="yamir2"
+NS1_ADDR="172.0.0.10/24"
+NS2_ADDR="172.0.0.20/24"
+BRIDGE="mac-wlan0"
+
+set -x
+
+# need interface for MACVLAN Master
+ip link add $BRIDGE type dummy
+ip link set $BRIDGE up
+
+ip netns add $NS1
+ip link add link $BRIDGE name mv1 type macvlan mode bridge
+ip link set mv1 netns $NS1
+ip netns exec $NS1 ip link set mv1 name wlan0
+ip netns exec $NS1 ip addr add $NS1_ADDR dev wlan0
+
+
+ip netns add $NS2
+ip link add link $BRIDGE name mv1 type macvlan mode bridge
+ip link set mv1 netns $NS2
+ip netns exec $NS2 ip link set mv1 name wlan0
+ip netns exec $NS2 ip addr add $NS2_ADDR dev wlan0
+
+echo "$NS1 and $NS2 are up"
+
+# load module
+insmod $KYAMIR ifname=wlan0
+
+ip netns exec $NS1 $YAMIRD -d -i wlan0
+ip netns exec $NS2 $YAMIRD -d -i wlan0
